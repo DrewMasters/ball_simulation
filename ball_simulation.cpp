271a1572df9fbf2f5fbd601be539d10ca275d6de
Drew Masters <dmaster1@lap167.local>
2017-04-26 17:41:57 -0400

    put server and client connect functions in server.c/h and put server read/write function in server.c/h

--- ball_simulation.cpp
+++ ball_simulation.cpp
@@ -6,12 +6,15 @@
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <pthread.h>
+#include "server.h"
 using namespace std;
 
 void *task(void *);
+void read_write_socket(int c);
 static int clifd;
 static char buffer[256];
 
@@ -19,7 +22,7 @@ int main(int argc, char **argv){
 	int i,port, sock, sockfd;//, clifd;
 	socklen_t clilen;
 	//char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
+	struct sockaddr_in  cli_addr;
 	struct hostent *server;
 	string type(argv[1]);
 	pthread_t *threads;
@@ -43,31 +46,8 @@ int main(int argc, char **argv){
 
 	//server code
 	if (type == "server"){
-		//creates an endpoint for communication
-		//and returns a file descriptor to end point
-		//	AF_INET= IPv4 Internet protocols
-		//	SOCK_STREAM = sequenced, reliable, 2-way, connection-based byte streams
-		//	0 = operating system will choose appropriate protocol
-		//		TCP because it is a streaming socket
-		sockfd = socket(AF_INET, SOCK_STREAM, 0);
-		if (sockfd < 0){
-			fprintf(stderr, "error opening socket\n");
-		}
-		//initialize serv_addr buffer to zero
-		bzero((char *) &serv_addr, sizeof(serv_addr));
-		//setting values in sockaddr_in struct
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_addr.s_addr = INADDR_ANY;
-		//htons converts port number to network byte order
-		serv_addr.sin_port = htons(port);
-
-		if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-			fprintf(stderr, "Error binding to port\n");
-			exit(1);
-		}
-		
-		//listens on socket for connecetions
-		listen(sockfd, 5);
+		sockfd = start_server(port);
+		cout << "started server " << sockfd << endl;
 		clilen = sizeof(cli_addr);
 		//blocks until client connects to server
 		//returns new file descriptor after connection
@@ -83,20 +63,8 @@ int main(int argc, char **argv){
 		for (i=0; i<3; i++) pthread_join(threads[i],NULL);
 	}
 	else if (type == "client"){
-		sockfd = socket(AF_INET, SOCK_STREAM,0);
-		if (sockfd < 0){
-			fprintf(stderr, "error opening socket\n");
-		}
-		server = gethostbyname(argv[3]);
-		if (server == NULL){
-			fprintf(stderr, "error no such host\n");
-			exit(0);
-		}
-		bzero((char *) &serv_addr, sizeof(serv_addr));
-		serv_addr.sin_family = AF_INET;
-		bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-		serv_addr.sin_port = htons(port);
-		if (::connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0 ) fprintf(stderr, "error connecting to host\n");
+		sockfd = start_client(port,argv[3]);
+		cout << "started client " << sockfd << endl;
 		if (read(sockfd, buffer, 256)<0)fprintf(stderr, "Error reading from socket\n");
 		printf("%s",buffer);
 		sprintf(buffer,"client connected\n");
@@ -105,6 +73,8 @@ int main(int argc, char **argv){
 		if (read(sockfd, buffer, 256)<0)fprintf(stderr, "Error reading from socket\n");
 		buffer[255]='\0';
 		printf("%s",buffer);
+		
+		//read_write_socket(sockfd);
 		close(sockfd);
 	}
 
@@ -112,19 +82,5 @@ int main(int argc, char **argv){
 }
 
 void *task(void *){
-	cout << pthread_self() << endl;
-	sprintf(buffer, "Server: connected\n");
-	write(clifd,buffer,strlen(buffer));
-	bzero(buffer, 256);
-	if (read(clifd,buffer,256) < 0) {
-		fprintf(stderr, "Error reading from socket\n");
-		exit(1);
-	}
-	cout << "in thread " <<pthread_self() << endl;
-	printf("%s",buffer);
-	if (write(clifd,"Got your message\n",17)<0){
-		fprintf(stderr, "error writing to socket\n");
-		exit(1);
-	}
-	close(clifd);
+	read_write_socket(clifd);
 }
---
+++ server.h
@@ -0,0 +1,3 @@
+int start_server(int port);
+int start_client(int port, char *host);
+void read_write_socket(int c);
